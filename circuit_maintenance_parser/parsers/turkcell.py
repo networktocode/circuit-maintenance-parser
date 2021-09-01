6ad133a77b6020397d7eb6d89984f8f4697ebace
carbonarok <carbonarok@gmail.com>
2021-09-01 14:33:34 +0100

    Moved second table parsing to elif. Added doc string.

--- circuit_maintenance_parser/parsers/turkcell.py
+++ circuit_maintenance_parser/parsers/turkcell.py
@@ -28,7 +28,70 @@ class HtmlParserTurkcell1(Html):
             raise ParsingError from exc
 
     def parse_tables(self, tables: ResultSet, data: Dict):
-        """Parse tables."""
+        """Parse tables.
+
+        Format 1 supported here (all information in a single table):
+            <table>
+                <tr>
+                    <td>Dear Customer ... planned ...</td>
+                    <td>...</td>
+                </tr>
+                <tr>
+                    <td>Maintenance Number</td>
+                    <td>123456789</td>
+                </tr>
+                <tr>
+                    <td>Start</td>
+                    <td>2021-08-31 01:02:03</td>
+                </td>
+                <tr>
+                    <td>End</td>
+                    <td>2021-08-31 01:02:04</td>
+                </td>
+                <tr>
+                    <td>Impact of the maintenance</td>
+                    <td>
+                        <p>CIRCUIT1|01-CUSTOMER|LOCATION|LINK</p>
+                        <p>CIRCUIT2|01-CUSTOMER|LOCATION|LINK</p>
+                    </td>
+                </td>
+            </table>
+
+        Format #2 supported here (one table for general information, a separate table listing impacted circuits):
+            <table>
+                <tr>
+                    <td>Dear Customer ... planned ...</td>
+                    <td>...</td>
+                </tr>
+                <tr>
+                    <td>Maintenance Number</td>
+                    <td>123456789</td>
+                </tr>
+                <tr>
+                    <td>Start</td>
+                    <td>2021-08-31 01:02:03</td>
+                </td>
+                <tr>
+                    <td>End</td>
+                    <td>2021-08-31 01:02:04</td>
+                </td>
+                <tr>
+                    <td>Impact of the maintenance</td>
+                    <table>
+                        <tr>
+                            <td>Circuit 1</td>
+                            <td>Custom Name</td>
+                            <td>Down</td>
+                        </tr>
+                        <tr>
+                            <td>Circuit 2</td>
+                            <td>Custom Name</td>
+                            <td>Down</td>
+                        </tr>
+                    </table>
+                </td>
+            </table>
+        """
         # Main table
         td_elements = tables[0].find_all("td")
         for idx, td_element in enumerate(td_elements):
@@ -51,19 +114,18 @@ class HtmlParserTurkcell1(Html):
                     for element in p_elements:
                         # Example match:
                         #   Eth-Trunk1.1               up      up       111111111111111|01-CUSTOMER|LOCATION|LINK
-                        if re.match(r".+[ \t]([0-1]+\|.+\|.+\|.+)", element.text.strip()):
-                            groups = re.search(r".+[ \t]([0-1]+\|.+\|.+\|.+)", element.text.strip())
-                            if groups:
-                                details = groups.group(1).split("|")
-                                data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=details[0]))
-                                data["account"] = details[1]
+                        groups = re.search(r".+[ \t]([0-1]+\|.+\|.+\|.+)", element.text.strip())
+                        if groups:
+                            details = groups.group(1).split("|")
+                            data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=details[0]))
+                            data["account"] = details[1]
 
-        # Circuit table
-        # Possibility that there could be a table inside the first table.
-        if len(tables) == 2:
-            tr_elements = tables[1].find_all("tr")
-            data["circuits"] = []
-            for tr_element in tr_elements:
-                line = tr_element.text.strip().split("\n\n\n")
-                data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=line[0]))
-                data["account"] = line[1]
+                # Circuit table
+                # Possibility that there could be a table inside the first table.
+                elif len(tables) == 2:
+                    tr_elements = tables[1].find_all("tr")
+                    data["circuits"] = []
+                    for tr_element in tr_elements:
+                        line = tr_element.text.strip().split("\n\n\n")
+                        data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=line[0]))
+                        data["account"] = line[1]
