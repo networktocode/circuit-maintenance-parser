c279e2516d66dcd608dc7c7e5d3f34509ab7fcfb
Kristian Carbonaro <carbonarok@gmail.com>
2021-09-06 16:42:14 +0100

    Momentum Provider (#66)
    * Momentum parser.

--- tests/unit/test_e2e.py
+++ tests/unit/test_e2e.py
@@ -17,6 +17,7 @@ from circuit_maintenance_parser.provider import (
     Lumen,
     Megaport,
     NTT,
+    Momentum,
     PacketFabric,
     Seaborn,
     Telia,
@@ -107,6 +108,12 @@ GENERIC_ICAL_RESULT_PATH = Path(dir_path, "data", "ical", "ical1_result.json")
                 Path(dir_path, "data", "megaport", "megaport2_result.json"),
                 Path(dir_path, "data", "date", "email_date_1_result.json"),
             ],
+        ),
+        # Momentum
+        (
+            Momentum,
+            [("email", Path(dir_path, "data", "momentum", "momentum1.eml")),],
+            [Path(dir_path, "data", "momentum", "momentum1_result.json"),],
         ),
         # NTT
         (
--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -10,6 +10,7 @@ from circuit_maintenance_parser.parsers.cogent import HtmlParserCogent1
 from circuit_maintenance_parser.parsers.gtt import HtmlParserGTT1
 from circuit_maintenance_parser.parsers.lumen import HtmlParserLumen1
 from circuit_maintenance_parser.parsers.megaport import HtmlParserMegaport1
+from circuit_maintenance_parser.parsers.momentum import HtmlParserMomentum1
 from circuit_maintenance_parser.parsers.seaborn import (
     HtmlParserSeaborn1,
     HtmlParserSeaborn2,
@@ -87,6 +88,12 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
             HtmlParserMegaport1,
             Path(dir_path, "data", "megaport", "megaport2.html"),
             Path(dir_path, "data", "megaport", "megaport2_result.json"),
+        ),
+        # Momentum
+        (
+            HtmlParserMomentum1,
+            Path(dir_path, "data", "momentum", "momentum1.eml"),
+            Path(dir_path, "data", "momentum", "momentum1_html_parser_result.json"),
         ),
         # NTT
         (ICal, Path(dir_path, "data", "ntt", "ntt1"), Path(dir_path, "data", "ntt", "ntt1_result.json"),),
