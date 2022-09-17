3cf8ce46e5cc9e80df5839d797e5f2c27bd01eaa
Christian Adell <chadell@gmail.com>
2022-09-17 19:26:05 +0200

    Clean up IPv6 addresses too

--- anonymize_public_ips.py
+++ anonymize_public_ips.py
@@ -5,10 +5,13 @@ import sys
 import argparse
 import fileinput
 
-# TODO: refine better ipv4 regex
-IPV4_ADDR_REGEX = re.compile(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
-REPLACE_TEXT = "192.0.2.1"
 TEST_DATA_PATH = os.path.join("tests", "unit", "data")
+REPLACE_TEXT_IPV4 = "192.0.2.1"
+REPLACE_TEXT_IPV6 = "2001:DB8::1"
+IPV4_ADDR_REGEX = re.compile(
+    r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+)
+IPV6_ADDR_REGEX = re.compile(r"(?<![:.\w])(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}(?![:.\w])")
 
 
 def replace(filename, search_exp, replace_exp):
@@ -41,19 +44,24 @@ def main():
                     with open(filename, "r", encoding="utf-8") as test_file:
                         content = test_file.readlines()
                         for content_line in content:
-                            content_new = re.sub(IPV4_ADDR_REGEX, REPLACE_TEXT, content_line)
+                            content_new = re.sub(IPV4_ADDR_REGEX, REPLACE_TEXT_IPV4, content_line)
+                            content_new = re.sub(IPV6_ADDR_REGEX, REPLACE_TEXT_IPV6, content_new)
                             if content_line != content_new:
                                 if not report:
                                     report = f"IP Address found in {filename}\n"
                                 report += f"  - {content_line}"
                 else:
-                    report = replace(filename, IPV4_ADDR_REGEX, REPLACE_TEXT)
+                    report = replace(filename, IPV4_ADDR_REGEX, REPLACE_TEXT_IPV4)
+                    report += replace(filename, IPV6_ADDR_REGEX, REPLACE_TEXT_IPV6)
                 if report:
                     ip_found = True
                     print(report)
 
-    if ip_found:
+    if ip_found and not clean:
+        print("\nHINT - you can clean up these IPs with 'invoke clean-anonymize-ips'")
         sys.exit(1)
+    elif ip_found and clean:
+        print(f"\nIP addresses have been changed to {REPLACE_TEXT_IPV4} and {REPLACE_TEXT_IPV6}")
     else:
         print("Only documentation IPs found.")
 
