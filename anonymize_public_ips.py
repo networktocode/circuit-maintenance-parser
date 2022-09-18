35c5ec5a9c54c87147d17f63eff4d2f6a3013888
Christian Adell <chadell@gmail.com>
2022-09-18 19:03:19 +0200

    Improve IPv6 regex and clean up

--- anonymize_public_ips.py
+++ anonymize_public_ips.py
@@ -8,10 +8,33 @@ import fileinput
 TEST_DATA_PATH = os.path.join("tests", "unit", "data")
 REPLACE_TEXT_IPV4 = "192.0.2.1"
 REPLACE_TEXT_IPV6 = "2001:DB8::1"
-IPV4_ADDR_REGEX = re.compile(
-    r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+
+
+# Reference: https://gist.github.com/dfee/6ed3a4b05cfe7a6faf40a2102408d5d8
+IPV4SEG = r"(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])"
+IPV4_ADDR_REGEX = r"(?:(?:" + IPV4SEG + r"\.){3,3}" + IPV4SEG + r")"
+IPV6SEG = r"(?:(?:[0-9a-fA-F]){1,4})"
+IPV6GROUPS = (
+    r"(?:" + IPV6SEG + r":){7,7}" + IPV6SEG,  # 1:2:3:4:5:6:7:8
+    r"(?:" + IPV6SEG + r":){1,7}:",  # 1::                                 1:2:3:4:5:6:7::
+    r"(?:" + IPV6SEG + r":){1,6}:" + IPV6SEG,  # 1::8               1:2:3:4:5:6::8   1:2:3:4:5:6::8
+    r"(?:" + IPV6SEG + r":){1,5}(?::" + IPV6SEG + r"){1,2}",  # 1::7:8             1:2:3:4:5::7:8   1:2:3:4:5::8
+    r"(?:" + IPV6SEG + r":){1,4}(?::" + IPV6SEG + r"){1,3}",  # 1::6:7:8           1:2:3:4::6:7:8   1:2:3:4::8
+    r"(?:" + IPV6SEG + r":){1,3}(?::" + IPV6SEG + r"){1,4}",  # 1::5:6:7:8         1:2:3::5:6:7:8   1:2:3::8
+    r"(?:" + IPV6SEG + r":){1,2}(?::" + IPV6SEG + r"){1,5}",  # 1::4:5:6:7:8       1:2::4:5:6:7:8   1:2::8
+    IPV6SEG + r":(?:(?::" + IPV6SEG + r"){1,6})",  # 1::3:4:5:6:7:8     1::3:4:5:6:7:8   1::8
+    r":(?:(?::" + IPV6SEG + r"){1,7}|:)",  # ::2:3:4:5:6:7:8    ::2:3:4:5:6:7:8  ::8       ::
+    r"fe80:(?::"
+    + IPV6SEG
+    + r"){0,4}%[0-9a-zA-Z]{1,}",  # fe80::7:8%eth0     fe80::7:8%1  (link-local IPv6 addresses with zone index)
+    r"::(?:ffff(?::0{1,4}){0,1}:){0,1}[^\s:]"
+    + IPV4_ADDR_REGEX,  # ::255.255.255.255  ::ffff:255.255.255.255  ::ffff:0:255.255.255.255 (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
+    r"(?:"
+    + IPV6SEG
+    + r":){1,4}:[^\s:]"
+    + IPV4_ADDR_REGEX,  # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
 )
-IPV6_ADDR_REGEX = re.compile(r"(?<![:.\w])(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}(?![:.\w])")
+IPV6_ADDR_REGEX = "|".join([f"(?:{g})" for g in IPV6GROUPS[::-1]])  # Reverse rows for greedy match
 
 
 def replace(filename, search_exp, replace_exp):
