cb5f5f765f072a711c7d09842aa08a69ed550dff
Christian Adell <chadell@gmail.com>
2022-09-24 09:01:46 +0200

    skip mypy check for netconan

--- anonymize_ips.py
+++ anonymize_ips.py
@@ -4,17 +4,17 @@ import sys
 import fileinput
 import random
 import string
-from netconan.ip_anonymization import IpAnonymizer, IpV6Anonymizer, anonymize_ip_addr
+from netconan.ip_anonymization import IpAnonymizer, IpV6Anonymizer, anonymize_ip_addr  # type: ignore
 
 
 TEST_DATA_PATH = os.path.join("tests", "unit", "data")
 
 _DEFAULT_SALT_LENGTH = 16
 _CHAR_CHOICES = string.ascii_letters + string.digits
-salt = "".join(random.choice(_CHAR_CHOICES) for _ in range(_DEFAULT_SALT_LENGTH))  # nosec
+SALT = "".join(random.choice(_CHAR_CHOICES) for _ in range(_DEFAULT_SALT_LENGTH))  # nosec
 
-anonymizer4 = IpAnonymizer(salt, None, preserve_addresses=["192.0.2.0/24"], preserve_suffix=None,)
-anonymizer6 = IpV6Anonymizer(salt, preserve_suffix=None)
+anonymizer4 = IpAnonymizer(SALT, None, preserve_addresses=["192.0.2.0/24"], preserve_suffix=None,)
+anonymizer6 = IpV6Anonymizer(SALT, preserve_suffix=None)
 
 
 def replace(filename):
