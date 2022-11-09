316c8cdafcd696737785a17ee55d1752de6ff55d
unpapillon <80388812+unpapillon@users.noreply.github.com>
2022-11-09 10:31:06 +0100

    Update Telstra.py for new notification format

--- circuit_maintenance_parser/parsers/telstra.py
+++ circuit_maintenance_parser/parsers/telstra.py
@@ -6,6 +6,7 @@ from dateutil import parser
 from bs4.element import ResultSet  # type: ignore
 
 from circuit_maintenance_parser.parser import Html, Impact, CircuitImpact, Status
+import re
 
 # pylint: disable=too-many-branches
 
@@ -25,51 +26,62 @@ class HtmlParserTelstra1(Html):
     def parse_tables(self, tables: ResultSet, data: Dict):  # pylint: disable=too-many-locals
         """Parse Table tag."""
         for table in tables:
-            for td_element in table.find_all("td"):
+            for td_element in table.find_all("p"):
                 # TODO: We should find a more consistent way to parse the status of a maintenance note
                 td_text = td_element.text.lower()
+                print(td_text)
                 if "maintenance has been scheduled" in td_text:
                     data["status"] = Status("CONFIRMED")
-                elif "this is a reminder notification to notify that a planned maintenance" in td_text:
-                    data["status"] = Status("CONFIRMED")
-                elif "has been completed" in td_text:
+                elif "successful" in td_text:
                     data["status"] = Status("COMPLETED")
-                elif "has been amended" in td_text:
+                elif (
+                    "this is a note to let you know that we'll soon be doing some network maintenance"
+                    in td_text
+                ):
+                    data["status"] = Status("CONFIRMED")
+                elif "has been rescheduled" in td_text:
                     data["status"] = Status("RE-SCHEDULED")
                 elif "has been withdrawn" in td_text or "has been cancelled" in td_text:
                     data["status"] = Status("CANCELLED")
                 else:
                     continue
                 break
-            for th_element in table.find_all("th"):
+            for th_element in table.find_all("strong"):
                 if not th_element.string:
                     continue
                 th_text = th_element.string.strip()
                 th_sibling = th_element.next_sibling.next_sibling
-                if th_text == "To:":
-                    data["account"] = th_sibling.string
-                elif th_text == "Change Reference:":
+                data["account"] = "Criteo SA (EMEA)"
+                if th_text == "Reference number":
                     data["maintenance_id"] = th_sibling.string
-                elif th_text == "Maintenance Window:":
-                    text_dates = th_sibling.string.split("(UTC) to ")
-                    start = parser.parse(text_dates[0])
+                elif th_text == "Start time":
+                    text_start = th_sibling.string
+                    regex = re.search(
+                        "\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_start
+                    )
+                    start = parser.parse(regex.group())
                     data["start"] = self.dt2ts(start)
-                    end = parser.parse(text_dates[1].strip("(UTC)"))
+                elif th_text == "End time":
+                    text_end = th_sibling.string
+                    regex = re.search("\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_end)
+                    end = parser.parse(regex.group())
                     data["end"] = self.dt2ts(end)
-                elif th_text == "Service(s) Impacted:":
+                elif th_text == "Service/s under maintenance":
                     data["circuits"] = []
                     # TODO: This split is just an assumption of the multiple service, to be checked with more samples
                     impacted_circuits = th_sibling.text.split(", ")
                     for circuit_id in impacted_circuits:
-                        data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=circuit_id))
-                elif th_text == "Maintenance Details:":
+                        data["circuits"].append(
+                            CircuitImpact(impact=Impact("OUTAGE"), circuit_id=circuit_id)
+                        )
+                elif th_text == "Maintenance Details":
                     sentences: List[str] = []
                     for element in th_element.next_elements:
-                        if element.string == "Service(s) Impacted:":
+                        if element.string == "Reference number":
                             break
                         if element.string and element.string not in ["\n", "", "\xa0"] + sentences:
                             sentences.append(element.string)
                     if sentences:
-                        # First sentence containts 'Maintenance Details:' so we skip it
+                        # First sentence containts 'Maintenance Details' so we skip it
                         data["summary"] = ". ".join(sentences[1:])
             break
