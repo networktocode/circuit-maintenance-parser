069e8842a2d4e2e95df523d5daddad21b5bf038d
c.radet <c.radet@criteo.com>
2022-11-23 09:47:24 +0100

    fix pylint

--- circuit_maintenance_parser/parsers/telstra.py
+++ circuit_maintenance_parser/parsers/telstra.py
@@ -23,6 +23,48 @@ class HtmlParserTelstra1(Html):
         self.parse_tables(soup.find_all("table"), data)
         return [data]
 
+    def add_maintenance_data(self, table: ResultSet, data: Dict):
+        """populate data dict"""
+        for strong_element in table.find_all("strong"):
+            if not strong_element.string:
+                continue
+            strong_text = strong_element.string.strip()
+            strong_sibling = strong_element.next_sibling.next_sibling
+            if strong_text == "Reference number":
+                data["maintenance_id"] = strong_sibling.string.strip()
+            elif strong_text == "Start time":
+                text_start = strong_sibling.string
+                regex = re.search(r"\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_start)
+                if regex is not None:
+                    start = parser.parse(regex.group())
+                    data["start"] = self.dt2ts(start)
+                else:
+                    data["start"] = "Not defined"
+            elif strong_text == "End time":
+                text_end = strong_sibling.string
+                regex = re.search(r"\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_end)
+                if regex is not None:
+                    end = parser.parse(regex.group())
+                    data["end"] = self.dt2ts(end)
+                else:
+                    data["end"] = "is not defined"
+            elif strong_text == "Service/s under maintenance":
+                data["circuits"] = []
+                # TODO: This split is just an assumption of the multiple service, to be checked with more samples
+                impacted_circuits = strong_sibling.text.split(", ")
+                for circuit_id in impacted_circuits:
+                    data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=circuit_id.strip()))
+            elif strong_text == "Maintenance Details":
+                sentences: List[str] = []
+                for element in strong_element.next_elements:
+                    if element.string == "Reference number":
+                        break
+                    if element.string and element.string not in ["\n", "", "\xa0"] + sentences:
+                        sentences.append(element.string)
+                if sentences:
+                    # First sentence containts 'Maintenance Details' so we skip it
+                    data["summary"] = ". ".join(sentences[1:])
+
     def parse_tables(self, tables: ResultSet, data: Dict):  # pylint: disable=too-many-locals
         """Parse Table tag."""
         for table in tables:
@@ -48,43 +90,5 @@ class HtmlParserTelstra1(Html):
                 else:
                     continue
                 break
-            for strong_element in table.find_all("strong"):
-                if not strong_element.string:
-                    continue
-                strong_text = strong_element.string.strip()
-                strong_sibling = strong_element.next_sibling.next_sibling
-                if strong_text == "Reference number":
-                    data["maintenance_id"] = strong_sibling.string.strip()
-                elif strong_text == "Start time":
-                    text_start = strong_sibling.string
-                    regex = re.search(r"\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_start)
-                    if regex is not None:
-                        start = parser.parse(regex.group())
-                        data["start"] = self.dt2ts(start)
-                    else:
-                        data["start"] = "Not defined"
-                elif strong_text == "End time":
-                    text_end = strong_sibling.string
-                    regex = re.search(r"\d{2}\s[a-zA-Z]{3}\s\d{4}\s\d{2}[:]\d{2}[:]\d{2}", text_end)
-                    if regex is not None:
-                        end = parser.parse(regex.group())
-                        data["end"] = self.dt2ts(end)
-                    else:
-                        data["end"] = "is not defined"
-                elif strong_text == "Service/s under maintenance":
-                    data["circuits"] = []
-                    # TODO: This split is just an assumption of the multiple service, to be checked with more samples
-                    impacted_circuits = strong_sibling.text.split(", ")
-                    for circuit_id in impacted_circuits:
-                        data["circuits"].append(CircuitImpact(impact=Impact("OUTAGE"), circuit_id=circuit_id.strip()))
-                elif strong_text == "Maintenance Details":
-                    sentences: List[str] = []
-                    for element in strong_element.next_elements:
-                        if element.string == "Reference number":
-                            break
-                        if element.string and element.string not in ["\n", "", "\xa0"] + sentences:
-                            sentences.append(element.string)
-                    if sentences:
-                        # First sentence containts 'Maintenance Details' so we skip it
-                        data["summary"] = ". ".join(sentences[1:])
+            self.add_maintenance_data(table, data)
             break
