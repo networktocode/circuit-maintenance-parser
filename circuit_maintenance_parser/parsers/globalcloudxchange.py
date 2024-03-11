f993d595c34a28e71320e205cfbab7a70dcc79a9
Nate Gotz <775979+nlgotz@users.noreply.github.com>
2024-03-11 11:55:35 -0500

    chore: fix test failures

--- circuit_maintenance_parser/parsers/globalcloudxchange.py
+++ circuit_maintenance_parser/parsers/globalcloudxchange.py
@@ -5,9 +5,7 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 from bs4.element import ResultSet  # type: ignore
-from dateutil import parser
 
-from circuit_maintenance_parser.output import Impact
 from circuit_maintenance_parser.parser import EmailSubjectParser, Html, Status
 
 
@@ -15,7 +13,7 @@ class HtmlParserGcx1(Html):
     """Custom Parser for HTML portion of Global Cloud Xchange circuit maintenance notifications."""
 
     def parse_html(self, soup: ResultSet) -> List[Dict]:
-        """Parse an equinix circuit maintenance email.
+        """Parse an Global Cloud Xchange circuit maintenance email.
 
         Args:
             soup (ResultSet): beautiful soup object containing the html portion of an email.
@@ -44,115 +42,6 @@ class HtmlParserGcx1(Html):
 
         return [data]
 
-    @staticmethod
-    def _isascii(string):
-        """Python 3.6 compatible way to determine if string is only english characters.
-
-        Args:
-            string (str): string to test if only ascii chars.
-
-        Returns:
-            bool: Returns True if string is ascii only, returns false if the string contains extended unicode characters.
-        """
-        try:
-            string.encode("ascii")
-            return True
-        except UnicodeEncodeError:
-            return False
-
-    def _parse_bolded(self, b_elements, data):
-        """Parse the <b> / <strong> elements from the notification to capture start and end times, description, and impact.
-
-        Args:
-            b_elements (): resulting soup object with all <b> elements
-            data (Dict): data from the circuit maintenance
-
-        Returns:
-            impact (Status object): impact of the maintenance notification (used in the parse table function to assign an impact for each circuit).
-        """
-        impact = None
-        start_year = 0
-        end_year = 0
-        for b_elem in b_elements:
-            if "SPAN:" in b_elem.text:
-                # Formated in DAY MONTH YEAR
-                # *SPAN: 02-JUL-2021 - 03-JUL-2021*
-                raw_year_span = b_elem.text.strip().split()
-                start_year = raw_year_span[1].split("-")[-1]
-                end_year = raw_year_span[-1].split("-")[-1]
-            if start_year != 0 and "UTC:" in b_elem.text:
-                raw_time = b_elem.next_sibling
-                # for non english equinix notifications
-                # english section is usually at the bottom
-                # this skips the non english line at the top
-                if not self._isascii(raw_time):
-                    continue
-
-                # Expected Format *UTC:* FRIDAY, 02 JUL 10:00 - FRIDAY, 02 JUL 15:00
-                # Note this detailed time does not contain the year..
-                start_end_time = raw_time.split("-")
-                if len(start_end_time) == 2:
-                    data["start"] = self.dt2ts(parser.parse(start_end_time[0].strip() + f" {start_year}"))
-                    data["end"] = self.dt2ts(parser.parse(start_end_time[1].strip() + f" {end_year}"))
-            # all circuits in the notification share the same impact
-            if "IMPACT:" in b_elem:
-                impact_line = b_elem.next_sibling
-                impact_sibling_line = (impact_line.next_sibling and impact_line.next_sibling.text) or ""
-
-                if "No impact to your service" in impact_line:
-                    impact = Impact.NO_IMPACT
-                elif "There will be service interruptions" in impact_line:
-                    impact = Impact.OUTAGE
-                elif "There will be service interruptions" in impact_sibling_line:
-                    impact = Impact.OUTAGE
-                elif "Loss of redundancy" in impact_line:
-                    impact = Impact.REDUCED_REDUNDANCY
-                elif "Traffic will be re-routed" in impact_line:
-                    impact = Impact.REDUCED_REDUNDANCY
-        return impact
-
-    def _parse_table(self, theader_elements, data, impact):
-        for th_elem in theader_elements:
-            if "Account #" in th_elem:
-                circuit_table = th_elem.find_parent("table")
-                for tr_elem in circuit_table.find_all("tr"):
-                    if tr_elem.find(th_elem):
-                        continue
-                    circuit_info = list(tr_elem.find_all("td"))
-                    if circuit_info:
-                        if len(circuit_info) == 4:
-                            # Equinix Connect notifications contain the IBX name
-                            account, _, _, circuit = circuit_info  # pylint: disable=unused-variable
-                        elif len(circuit_info) == 14:
-                            # Equinix Fabric notifications include a lot of additional detail on seller and subscriber ID's
-                            (
-                                account,
-                                _,
-                                _,
-                                circuit,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                                _,
-                            ) = circuit_info  # pylint: disable=unused-variable
-                        elif len(circuit_info) == 3:
-                            account, _, circuit = circuit_info  # pylint: disable=unused-variable
-                        else:
-                            return
-                        data["circuits"].append(
-                            {
-                                "circuit_id": circuit.text,
-                                "impact": impact,
-                            }
-                        )
-                        data["account"] = account.text
-
 
 class SubjectParserGcx1(EmailSubjectParser):
     """Parse the subject of a Global Cloud Xchange circuit maintenance email. The subject contains the maintenance ID and status."""
@@ -178,7 +67,7 @@ class SubjectParserGcx1(EmailSubjectParser):
             date_format = date_format = "%d-%b-%Y %H:%M"
             data["start"] = self.dt2ts(datetime.strptime(search.group(5), date_format))
             data["end"] = self.dt2ts(datetime.strptime(search.group(6), date_format))
-        data["summary"] = search.group(4)
+            data["summary"] = search.group(4)
         if "completed" in subject.lower():
             data["status"] = Status.COMPLETED
         elif "rescheduled" in subject.lower():
