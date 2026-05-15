fd46aaabe13bbf1596c375ccc8e857364e60f042
Arjan Koopen <arjankoopen@i3d.net>
2026-05-15 16:48:11 +0200

    Add changelog message, plus add equinix tests 9+10

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -371,6 +371,16 @@ class NestedEncoder(json.JSONEncoder):
             Path(dir_path, "data", "equinix", "equinix9.eml"),
             Path(dir_path, "data", "equinix", "equinix9_result.json"),
         ),
+        (
+            HtmlParserEquinix,
+            Path(dir_path, "data", "equinix", "equinix10.eml"),
+            Path(dir_path, "data", "equinix", "equinix10_result.json"),
+        ),
+        (
+            SubjectParserEquinix,
+            Path(dir_path, "data", "equinix", "equinix11_subject.eml"),
+            Path(dir_path, "data", "equinix", "equinix11_subject_result.json"),
+        ),
         # FLAG
         (
             HtmlParserFlag1,
